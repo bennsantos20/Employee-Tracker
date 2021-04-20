719f45f502a6fd09125d65f80b05a5530e1b7f0a
Benjamin <bennsantos20@gmail.com>
2021-04-20 01:00:34 -0400

    took out sql password

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Emiliosnts96!",
+  password: "",
   database: "employee_tracker_db",
 });
 
